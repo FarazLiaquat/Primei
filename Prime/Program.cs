4d382c9534eded06412aa2017764c1e55b351707
Faraz Liaquat <farazlikesfire@gmail.com>
2021-09-15 15:28:33 -0400

    Add Code

---
+++ Prime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z;
+            for (int x = 1; x <= 1000; x++)
+            {
+                z = 0;
+                for (int y = 1; y <= x; y++)
+                { 
+                    if (x % y == 0)
+                    {
+                        z++;
+                         
+                        
+                    }
+                }
+
+                if (z == 2)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Primei
+This program will output the numbers from 1 to 1000 in Jetbrains
